6ec0ab4592c5718ca6c609cf91a4ee12333c9043
Muhammad Awaab <muhammad.awaab92@gmail.com>
2022-08-15 08:32:49 +0500

    basic interface complete

--- ZoundTest/ZoundTest/View/ItemCell.swift
+++ ZoundTest/ZoundTest/View/ItemCell.swift
@@ -10,11 +10,23 @@ import UIKit
 class ItemCell: UITableViewCell {
 
     @IBOutlet weak var cryptoName: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var cryptoDescription: UILabel!
+    @IBOutlet weak var lowPriceLabel: UILabel!
+    @IBOutlet weak var highPriceLabel: UILabel!
+    @IBOutlet weak var changePercentageLabel: UILabel!
+    @IBOutlet weak var coloredView: UIView!
     
     
     var cellViewModel: ItemViewModelProtocol? {
         didSet {
             self.cryptoName.text = cellViewModel?.currencyNameForDisplay()
+            self.currencyLabel.text = cellViewModel?.getCurrencyLabel()
+            self.cryptoDescription.text = cellViewModel?.currencyDescForDisplay()
+            self.lowPriceLabel.text = cellViewModel?.getLowPriceToDipslay()
+            self.highPriceLabel.text = cellViewModel?.getHighPriceToDisplay()
+            self.changePercentageLabel.text = cellViewModel?.getCurrentOpenPrice()
+            self.coloredView.backgroundColor = cellViewModel?.isOpenPriceLowerThanLastPrice() ?? true ? #colorLiteral(red: 0.7882352941, green: 0, blue: 0.07843137255, alpha: 1) : #colorLiteral(red: 0, green: 0.7333333333, blue: 0.3647058824, alpha: 1)
         }
     }
    
--- ZoundTest/ZoundTest/ViewModel/ItemViewModel.swift
+++ ZoundTest/ZoundTest/ViewModel/ItemViewModel.swift
@@ -9,11 +9,49 @@ import Foundation
 
 protocol ItemViewModelProtocol {
     func currencyNameForDisplay() -> String
+    func currencyDescForDisplay() -> String
+    func getLowPriceToDipslay() -> String
+    func getHighPriceToDisplay() -> String
+    func isOpenPriceLowerThanLastPrice() -> Bool
+    func getCurrentOpenPrice() -> String
+    func getCurrencyLabel() -> String
 }
 
 class ItemViewModel: ItemViewModelProtocol {
+    func getCurrencyLabel() -> String {
+        if isDisplayingUSD {
+            return "/ USD"
+        } else {
+            return "/ SEK"
+        }
+    }
+    
+    func getCurrentOpenPrice() -> String {
+        return dataModelToDisplay.openPrice ?? ""
+    }
+    
+    func getLowPriceToDipslay() -> String {
+        return dataModelToDisplay.lowPrice ?? ""
+    }
+    
+    func getHighPriceToDisplay() -> String {
+        return dataModelToDisplay.highPrice ?? ""
+    }
+    
+    func isOpenPriceLowerThanLastPrice() -> Bool {
+        let openPrice = Double(dataModelToDisplay.openPrice ?? "0.0")!
+        let lastPrice = Double(dataModelToDisplay.lastPrice ?? "0.0")!
+        
+        if openPrice < lastPrice {
+            return true
+        } else {
+            return false
+        }
+    }
+    
     
     private let dataModelToDisplay: CryptoModel
+    var isDisplayingUSD: Bool = true
     
     init(dataModel: CryptoModel){
         self.dataModelToDisplay = dataModel
@@ -21,6 +59,10 @@ class ItemViewModel: ItemViewModelProtocol {
     
     func currencyNameForDisplay() -> String {
         return dataModelToDisplay.baseAsset?.uppercased() ?? ""
+    }
+    
+    func currencyDescForDisplay() -> String {
+        return dataModelToDisplay.symbol ?? ""
     }
     
 }
